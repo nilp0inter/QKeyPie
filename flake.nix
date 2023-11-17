cb8c5dfb1d86e41abe9a2ba6d7d14f88210251ce
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-11-17 21:51:05 +0100

    chore: flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "An open-source, Rust-based CLI tool for personalized key mapping and shortcut automation on Xencelabs Quick Keys";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        udev
+        xdotool
+      ];
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+      ];
+    };
+
+  };
+}
