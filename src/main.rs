676fc153553cccf07adac55d484fa5d0bbda2b07
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-11-17 22:47:23 +0100

    feat: rocket + qk control thread

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,76 @@
-extern crate xencelabs_quick_keys;
-
-extern crate hidapi;
-
-use std::{thread,time};
+use rocket::{get, routes};
+use rocket::tokio::task::spawn_blocking;
+use xencelabs_quick_keys::{QKDevice, ConnectionMode, QKResult, ScreenOrientation, ScreenBrightness, WheelSpeed, Event, ButtonState};
 use hidapi::HidApi;
+use std::{thread,time};
 use enigo::*;
 
-use xencelabs_quick_keys::*;
 
-/// This function is the main entry point for the example. It opens a connection to the device,
-/// and then sets the screen orientation, brightness, wheel speed, sleep timeout, ring color,
-/// and key text. It then enters a loop, reading events from the device and setting the ring
-/// color based on the wheel and buttons.
+struct SharedData {
+    // Add shared data fields here if necessary
+}
+
+#[get("/")]
+async fn index(_data: &rocket::State<SharedData>) -> String {
+    // Handle the HTTP request, e.g., returning a simple response
+    "Hello from Rocket!".to_string()
+}
+
+#[rocket::main]
+async fn main() {
+    let shared_data = SharedData {
+        // Initialize shared data fields here
+    };
+
+    // Spawn a blocking task for managing the Xencelabs Quick Keys device
+    tokio::spawn(async {
+        spawn_blocking(move || {
+            if let Ok(api) = HidApi::new() {
+                match run(api) {
+                    Ok(_) => println!("Device managed successfully"),
+                    Err(e) => println!("Error managing device: {:?}", e),
+                }
+            } else {
+                eprintln!("Error initializing HidApi");
+            }
+        }).await.unwrap();
+    });
+
+    // Launch the Rocket application
+    rocket::build()
+        .mount("/", routes![index])
+        .manage(shared_data)
+        .launch()
+        .await
+        .unwrap();
+}
+
 fn run(api: HidApi) -> QKResult<()> {
     let mut enigo = Enigo::new();
     match QKDevice::open(api, ConnectionMode::Auto) {
         Ok(dev) => {
-            dev.set_screen_orientation(ScreenOrientation::Rotate180)?;
+            // Set device settings such as orientation, brightness, etc.
+            dev.set_screen_orientation(ScreenOrientation::Rotate270)?;
             dev.set_screen_brightness(ScreenBrightness::Medium)?;
-            dev.set_wheel_speed(WheelSpeed::Slowest)?;
-            dev.set_sleep_timeout(1)?;
-            dev.set_ring_color(255, 255, 255)?;
-            dev.set_key_text(0, "red")?;
-            dev.set_key_text(1, "green")?;
-            dev.set_key_text(2, "blue")?;
-            dev.set_key_text(3, "yellow")?;
-            dev.set_key_text(4, "purple")?;
-            dev.set_key_text(5, "turquoise")?;
-            dev.set_key_text(6, "white")?;
-            dev.set_key_text(7, "off")?;
+            dev.set_wheel_speed(WheelSpeed::Faster)?;
+            dev.set_ring_color(0, 0, 0)?;
+
             thread::sleep(time::Duration::from_millis(1000));
-            dev.show_overlay_text("Disco, disco!", 3)?;
+            // Display "Hello World" on the device
+            dev.show_overlay_text("Hello World", 1)?;
             loop {
                 match dev.read() {
                     Ok(ev) => match ev {
-                        Event::Wheel { direction: WheelDirection::Left } => Ok(enigo.key_sequence_parse("{+META}{F11}{-META}")),
-                        Event::Wheel { direction: WheelDirection::Right } => Ok(enigo.key_sequence_parse("{+META}{F12}{-META}")),
-                        Event::Button { state: ButtonState { button_wheel: true, .. } } => dev.set_ring_color(0, 0, 255),
-                        Event::Button { state: ButtonState { button_0: true, .. } } => dev.set_ring_color(255, 0, 0),
-                        Event::Button { state: ButtonState { button_1: true, .. } } => dev.set_ring_color(0, 255, 0),
-                        Event::Button { state: ButtonState { button_2: true, .. } } => dev.set_ring_color(0, 0, 255),
-                        Event::Button { state: ButtonState { button_3: true, .. } } => dev.set_ring_color(255, 255, 0),
-                        Event::Button { state: ButtonState { button_4: true, .. } } => dev.set_ring_color(255, 0, 255),
-                        Event::Button { state: ButtonState { button_5: true, .. } } => dev.set_ring_color(0, 255, 255),
-                        Event::Button { state: ButtonState { button_6: true, .. } } => Ok(enigo.mouse_move_relative(10, 10)),
-                        Event::Button { state: ButtonState { button_7: true, .. } } => Ok(enigo.key_sequence_parse("{+CTRL}{F8}{-CTRL}")),
-                        Event::Button { state: ButtonState { button_extra: true, .. } } => dev.show_overlay_text("Disco, disco!", 3),
-                        Event::Button { state: ButtonState { .. } } => { println!("release"); Ok(()) },
-                        Event::Unknown { data: d } => { println!("unknown! {:?}", d); Ok(()) },
-                        Event::Battery { percent: p } => { println!("battery level: {:?}", p); Ok(()) },
+                        Event::Button { state: ButtonState { button_7: true, .. } } => Ok(enigo.key_sequence("hello world")),
+                        _ => Ok(()),
                     },
                     Err(e) => Err(e),
                 }?;
             }
         },
-        Err(e) => { println!("Connection error!"); Err(e) },
-    }
-}
-
-fn main() {
-    match HidApi::new() {
-        Ok(api) => match run(api) {
-            Ok(_) => { println!("all good"); }
-            Err(_) => { println!("error"); }
-        },
         Err(e) => {
-            eprintln!("Error: {}", e);
+            println!("Connection error: {:?}", e);
+            Err(e)
         },
     }
 }
