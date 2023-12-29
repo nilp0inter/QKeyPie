8395844ce6f8b65bac348139df64701a5c3fda59
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-12-29 21:42:26 +0100

    feat: flake package output

--- flake.nix
+++ flake.nix
@@ -19,5 +19,33 @@
       ];
     };
 
+    packages.x86_64-linux.qkeypie = pkgs.rustPlatform.buildRustPackage {
+      pname = "qkeypie";
+      version = "0.1.0";
+
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+
+      buildInputs = with pkgs; [
+        udev
+        xdotool
+      ];
+
+      src = ./.;
+
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+      };
+
+      meta = with nixpkgs.lib; {
+        description = "An open-source, Rust-based CLI tool for personalized key mapping and shortcut automation on Xencelabs Quick Keys";
+        homepage = "https://github.com/nilp0inter/QKeyPie";
+        license = licenses.gpl3;
+        platforms = platforms.linux;
+        maintainers = with maintainers; [ nilp0inter ];
+      };
+    };
+    packages.x86_64-linux.default = self.packages.x86_64-linux.qkeypie;
   };
 }
