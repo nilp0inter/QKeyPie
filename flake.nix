c8d68d24e9eec7a0464514e95d2e437bfd2b242b
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-12-29 21:58:17 +0100

    chore: flake-parts

--- flake.nix
+++ flake.nix
@@ -1,51 +1,80 @@
 {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+  };
+
   description = "An open-source, Rust-based CLI tool for personalized key mapping and shortcut automation on Xencelabs Quick Keys";
 
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-        udev
-        xdotool
-      ];
-      buildInputs = with pkgs; [
-        cargo
-        rustc
-        rustfmt
-        clippy
-      ];
-    };
-
-    packages.x86_64-linux.qkeypie = pkgs.rustPlatform.buildRustPackage {
-      pname = "qkeypie";
-      version = "0.1.0";
-
-      nativeBuildInputs = with pkgs; [
-        pkg-config
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
       ];
 
-      buildInputs = with pkgs; [
-        udev
-        xdotool
-      ];
+      systems = ["x86_64-linux" "aarch64-linux"];
 
-      src = ./.;
+      perSystem = {
+        self',
+        config,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            udev
+            xdotool
+          ];
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+          ];
+        };
 
-      cargoLock = {
-        lockFile = ./Cargo.lock;
+        overlayAttrs = {
+          inherit (config.packages) qkeypie;
+        };
+
+        packages.qkeypie = pkgs.rustPlatform.buildRustPackage {
+          pname = "qkeypie";
+          version = "0.1.0";
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            udev
+            xdotool
+          ];
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          meta = with nixpkgs.lib; {
+            description = "An open-source, Rust-based CLI tool for personalized key mapping and shortcut automation on Xencelabs Quick Keys";
+            homepage = "https://github.com/nilp0inter/QKeyPie";
+            license = licenses.gpl3;
+            platforms = platforms.linux;
+            maintainers = with maintainers; [nilp0inter];
+          };
+        };
+
+        packages.default = self'.packages.qkeypie;
+
+        formatter = pkgs.alejandra;
       };
-
-      meta = with nixpkgs.lib; {
-        description = "An open-source, Rust-based CLI tool for personalized key mapping and shortcut automation on Xencelabs Quick Keys";
-        homepage = "https://github.com/nilp0inter/QKeyPie";
-        license = licenses.gpl3;
-        platforms = platforms.linux;
-        maintainers = with maintainers; [ nilp0inter ];
-      };
     };
-    packages.x86_64-linux.default = self.packages.x86_64-linux.qkeypie;
-  };
 }
